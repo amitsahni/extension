493ad25fa695fd67c5daf8cede1c045212d53c4c
amit <amit@clickapps.co>
2019-04-05 09:24:15 +0530

    Min - 17 Log have to mention debug

--- extension/src/main/kotlin/com/extension/LogExtension.kt
+++ extension/src/main/kotlin/com/extension/LogExtension.kt
@@ -2,44 +2,45 @@
 package com.extension
 
 import android.util.Log
+import com.extension.Log.IS_DEBUG
 import com.extension.Log.TAG
-import java.util.logging.Logger
 
 object Log {
-    val TAG = Logger.getGlobal().javaClass.name
+    val TAG = "Logger"
+    var IS_DEBUG = true
 }
 fun String.printInfo() {
-    if (BuildConfig.DEBUG) {
+    if (IS_DEBUG) {
         Log.i(TAG, this)
     }
 }
 
 fun String.printError() {
-    if (BuildConfig.DEBUG) {
+    if (IS_DEBUG) {
         Log.e(TAG, this)
     }
 }
 
 fun String.printWarn() {
-    if (BuildConfig.DEBUG) {
+    if (IS_DEBUG) {
         Log.w(TAG, this)
     }
 }
 
 fun Any.printInfo() {
-    if (BuildConfig.DEBUG) {
+    if (IS_DEBUG) {
         Log.i(TAG, this.toString())
     }
 }
 
 fun Any.printError() {
-    if (BuildConfig.DEBUG) {
+    if (IS_DEBUG) {
         Log.e(TAG, this.toString())
     }
 }
 
 fun Any.printWarn() {
-    if (BuildConfig.DEBUG) {
+    if (IS_DEBUG) {
         Log.w(TAG, this.toString())
     }
 }
